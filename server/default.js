1ebcbccf3efaec5ecb52d43755de472d286af653
Aloknath-sah <--global>
2021-07-07 23:05:53 +0530

    productSchema added

--- server/default.js
+++ server/default.js
@@ -1,7 +1,14 @@
 import { products } from './constants/product.js'
+import product from './model/productSchema.js'
 
-const defaultData = () => {
-
+const defaultData = async () => {
+    try {
+        await product.deleteMany({});
+        await product.insertMany(products);
+        console.log("data imported successfully")
+    } catch(error) {
+        console.log("Error: ", error.message);
+    }
 }
 
 export default defaultData;
